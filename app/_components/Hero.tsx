c3ea021615fcb52aa27d45fffb136eac0848748a
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2026-02-21 12:24:37 +0800

    02.21 gertee

---
+++ app/_components/Hero.tsx
@@ -0,0 +1,11 @@
+export const Hero = () => {
+  return (
+    <div>
+      <div>
+        <h1>Таны дурсамжийг</h1>
+        <p>АМАР хялбараар, ТАЙВАН найдвартайгаар </p>
+        <button className="border">Towch</button>
+      </div>
+    </div>
+  );
+};
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { Hero } from "./_components/Hero";
+
+export default function Home() {
+  return (
+    <div className="bg-[#F8FAFC]">
+      <div className="mx-auto flex justify-center h-screen items-center">
+        <Hero />
+      </div>
+    </div>
+  );
+}
+
+// components/Hero.tsx: 1-р хэсэг буюу нүүр хуудасны дизайн.
+
+// components/PhotoGrid.tsx: Зургуудыг жагсааж харуулах хэсэг.
+
+// components/AIResult.tsx
